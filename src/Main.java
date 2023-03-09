33df2b9dfe42e304338d9eb2631c9d610f276ad3
Славик <slaviknaprimer@mail.ru>
2023-03-09 17:50:01 +0600

    Peredelka

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        int Balance = 300;
-        int EnterMany = 1100;
-        int Bonus = 0;
-        if (EnterMany > 1000) {
-            Bonus = EnterMany / 100;
+        int balance = 300;
+        int enterMany = 1100;
+        int bonus = 0;
+        if (enterMany > 1000) {
+            bonus = enterMany / 100;
         }
-        int Finish = Balance + EnterMany + Bonus;
-        System.out.println("Итоговый счёт" + " " + Finish);
-        System.out.println("Бонус" + " " + Bonus);
+        int finish = balance + enterMany + bonus;
+        System.out.println("Итоговый счёт" + " " + finish);
+        System.out.println("Бонус" + " " + bonus);
 
 
     }
