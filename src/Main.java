5022f4a10f11af8eb848f3bf95c3dd33cc42fbc6
Славик <slaviknaprimer@mail.ru>
2023-03-09 16:56:29 +0600

    Rabota

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int Balance = 300;
+        int EnterMany = 1100;
+        int Bonus = 0;
+        if (EnterMany > 1000) {
+            Bonus = EnterMany / 100;
+        }
+        int Finish = Balance + EnterMany + Bonus;
+        System.out.println("Итоговый счёт" + " " + Finish);
+        System.out.println("Бонус" + " " + Bonus);
+
+
     }
 }
